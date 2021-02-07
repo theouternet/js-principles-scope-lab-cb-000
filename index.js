79fbd1b87aa861dbe8e7cd6977ec0fc00dbd9542
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-07 20:34:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var customerName = "bob";
 
+const leastFavoriteCustomer = "test";
+
+
+
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(input) {
+  bestCustomer = input
+}
+
+function leastFavoriteCustomer(input) {
+  bestCustomer = input
+}
+
+function changeLeastFavoriteCustomer() {
+  
 }
